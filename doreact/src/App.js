5656297814565673c1ebc66bd589b06ebde5b9c6
bunn1 <nicjohbun@gmail.com>
2024-03-28 09:10:42 +0100

    Basic react installation

---
+++ doreact/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do List!</h1>
+      <div>
+        <input type="text" placeholer="..Task" />
+        <button>Add Task</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;

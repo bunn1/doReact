2ab9cab59c5a7691c7860c7af364b4c59dd22880
bunn1 <nicjohbun@gmail.com>
2024-04-11 20:38:12 +0200

    Removing todo list Inserting routes instead

--- doreact/src/App.js
+++ doreact/src/App.js
@@ -1,77 +1,38 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+import Home from './components/Home.js';
+import ToDo from './components/ToDo.js';
+import Login from './components/Login.js';
 
 function App() {
-  const [toDoList, setTodoList] = useState([]);
-  const [currentTask, setCurrentTask] = useState('');
-
-  const inputTask = useRef(null);
-
-  function addTask() {
-    setTodoList([...toDoList, { task: currentTask, completed: false }]);
-    inputTask.current.value = '';
-    setCurrentTask('');
-  }
-
-  const deleteTask = (taskToDelete) => {
-    setTodoList(
-      toDoList.filter((task) => {
-        return task.task !== taskToDelete;
-      })
-    );
-  };
-
-  const completeTask = (taskToComplete) => {
-    setTodoList(
-      toDoList.map((task) => {
-        return task.task === taskToComplete
-          ? { task: taskToComplete, completed: true }
-          : { task: task.task, completed: task.completed ? true : false };
-      })
-    );
-  };
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
 
   return (
     <div className="App">
-      <h1>To Do List!</h1>
-      <div>
-        <input
-          ref={inputTask}
-          value={currentTask}
-          type="text"
-          placeholer="..Task"
-          // Tryck på enter knappen för att lägga till en task
-          onKeyDown={(event) => {
-            event.keyCode === 13 && addTask();
-          }}
-          onChange={(event) => {
-            setCurrentTask(event.target.value);
-          }}
-        />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <hr />
-
-      <div>
-        <input type="text" />
-      </div>
-      <ul>
-        {toDoList.map((val, key) => {
-          return (
-            <div id="task">
-              <li key={key}>{val.task}</li>
-              <button onClick={() => completeTask(val.task)}>Completed</button>
-              <button onClick={() => deleteTask(val.task)}>X</button>
-              {val.completed ? (
-                <h1>Task Complete</h1>
-              ) : (
-                <h2>Task Not Completed</h2>
-              )}
-            </div>
-          );
-        })}
-      </ul>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route path="/todo" element={<ToDo />} />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;

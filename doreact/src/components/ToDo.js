c23e8f5d5e3de8f8171de1619609137dc8814edd
bunn1 <nicjohbun@gmail.com>
2024-04-13 09:21:23 +0200

    handleEdit function handleSave function

--- doreact/src/components/ToDo.js
+++ doreact/src/components/ToDo.js
@@ -4,6 +4,7 @@ import { useState, useRef } from 'react';
 function ToDo() {
   const [toDoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState('');
+  const [editTask, setEditTask] = useState(null);
 
   const inputTask = useRef(null);
 
@@ -33,6 +34,28 @@ function ToDo() {
     );
   };
 
+  const handleEdit = (task) => {
+    const foundTask = toDoList.find((t) => t.task === task);
+    if (foundTask) {
+      setEditTask(foundTask);
+      setCurrentTask(foundTask.task);
+    }
+  };
+
+  const handleSave = (task) => {
+    if (editTask) {
+      const updatedTasks = toDoList.map((task) => {
+        if (task.task === editTask.task) {
+          return { ...task, task: currentTask };
+        }
+        return task;
+      });
+      setTodoList(updatedTasks);
+      setEditTask(null);
+      setCurrentTask('');
+    }
+  };
+
   return (
     <div className="App">
       <h1>To Do List!</h1>
@@ -41,7 +64,7 @@ function ToDo() {
           ref={inputTask}
           value={currentTask}
           type="text"
-          placeholer="..Task"
+          placeholder="..Task"
           // Tryck på enter knappen för att lägga till en task
           onKeyDown={(event) => {
             event.keyCode === 13 && addTask();
@@ -63,11 +86,9 @@ function ToDo() {
                 {val.completed ? 'Mark Incomplete' : 'Mark Complete'}
               </button>
               <button onClick={() => deleteTask(val.task)}>X</button>
-              {val.completed ? (
-                <h1>Task Complete</h1>
-              ) : (
-                <h2>Task Not Completed</h2>
-              )}
+              <button onClick={() => handleEdit(val.task)}>Edit</button>
+              <button onClick={handleSave}>Save</button>
+              {val.task ? <h1>Task Complete</h1> : <h2>Task Not Completed</h2>}
             </div>
           );
         })}

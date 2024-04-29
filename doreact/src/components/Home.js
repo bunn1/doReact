e700dffefcb049d7727d86a5d4258ce22daa7e27
bunn1 <nicjohbun@gmail.com>
2024-04-29 16:32:22 +0200

    Styling the home page - mainContainer, titleContainer and the inputButton

--- doreact/src/components/Home.js
+++ doreact/src/components/Home.js
@@ -16,17 +16,17 @@ const Home = (props) => {
   };
 
   return (
-    <div className="mainContainer">
-      <div className={'titleContainer'}>
-        <h1>Welcome to the To Do App!</h1>
+    <div className="mainContainer h-screen bg-[#ccc5b9] flex flex-col justify-center items-center">
+      <div className="titleContainer p-5 shadow-md rounded-md max-w-md bg-[#fffcf2] text-3xl">
+        <h1 className="text-[#252422] mb-4 mt-4">Welcome to the To Do App!</h1>
       </div>
 
       <div className={'buttonContainer'}>
         <input
-          className={'inputButton'}
+          className="inputButton bg-[#eb5e28] rounded-md px-14 py-4 mt-14 text-white text-lg cursor-pointer hover:bg-[#d6492f]"
           type="button"
           onClick={handleButtonClick}
-          value={'To Do App Log In'}
+          value={'Log In Page'}
         />
         {loggedIn ? <div>Your email address is {email}</div> : <div />}
       </div>

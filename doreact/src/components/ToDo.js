a84cc4559422efa887e3a1849c57d7909b88b66f
bunn1 <nicjohbun@gmail.com>
2024-04-29 16:31:31 +0200

    Styling the todo main-container

--- doreact/src/components/ToDo.js
+++ doreact/src/components/ToDo.js
@@ -70,96 +70,101 @@ function ToDo() {
   };
 
   return (
-    <div className="container mx-auto p-4 shadow-md rounded-md max-w-md">
-      <h1 className="Rubrik text-2xl text-center pt-4 pb-4 ">To Do List!</h1>
-      <div className="flex flex-col justify-center items-center w-full h-full p-4">
-        <input
-          className="taskField border border-gray-300 rounded-md p-1  mb-4"
-          ref={inputTask}
-          value={currentTask}
-          type="text"
-          placeholder="..Task"
-          // Tryck på enter knappen för att lägga till en task
-          onKeyDown={(event) => {
-            event.keyCode === 13 && addTask();
-          }}
-          onChange={(event) => {
-            setCurrentTask(event.target.value);
-          }}
-        />
-        <div className="flex items-center">
-          <label className="labelReminder mr-4">Set Reminder:</label>
-          <div className="form-control form-control-check">
-            <input
-              className="checkReminder appearance-none w-5 h-5 border border-gray-300 rounded-md checked:bg-[#eb5e28] checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
-              name="field-name-three"
-              id="unique-field-id-three"
-              type="checkbox"
-              checked={reminder}
-              // value={reminder}
-              onChange={toggleReminder}
-            />
+    <div className="main-container min-h-screen bg-[#403d39] flex justify-center items-center">
+      <div className="container bg-[#fffcf2] mx-auto p-4 shadow-xl rounded-md max-w-lg">
+        <h1 className="Rubrik text-2xl text-[#252422] text-center pt-4 pb-4 ">
+          To Do List!
+        </h1>
+        <div className="flex flex-col justify-center items-center w-full h-full p-4">
+          <input
+            className="taskField border border-gray-300 rounded-md p-1  mb-4"
+            ref={inputTask}
+            value={currentTask}
+            type="text"
+            placeholder="..Task"
+            // Tryck på enter knappen för att lägga till en task
+            onKeyDown={(event) => {
+              event.keyCode === 13 && addTask();
+            }}
+            onChange={(event) => {
+              setCurrentTask(event.target.value);
+            }}
+          />
+          <div className="flex items-center">
+            <label className="labelReminder mr-4 text-[#252422]">
+              Set Reminder:
+            </label>
+            <div className="form-control form-control-check">
+              <input
+                className="checkReminder appearance-none w-5 h-5 border border-gray-300 rounded-md checked:bg-[#eb5e28] checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
+                name="field-name-three"
+                id="unique-field-id-three"
+                type="checkbox"
+                checked={reminder}
+                // value={reminder}
+                onChange={toggleReminder}
+              />
+            </div>
           </div>
+
+          <button
+            className="btnAdd bg-[#403d39] rounded-md px-4 py-2 mt-2 mb-2 text-white"
+            id="addButton"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
         </div>
 
-        <button
-          className="btnAdd bg-[#403d39] rounded-md px-4 py-2 mt-2 mb-2 text-white"
-          id="addButton"
-          onClick={addTask}
-        >
-          Add Task
-        </button>
-      </div>
-      <hr />
-
-      <div className="todo-list">
-        <ul className="mx-auto p-6 shadow-md rounded-md max-w-md ">
-          {toDoList.map((val, key) => {
-            return (
-              <div className="flex flex-col items-center" id="task" key={key}>
-                <li
-                  style={{
-                    textDecoration: val.completed ? 'line-through' : 'none',
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}
-                >
-                  {val.reminder && (
-                    <FiCheckCircle style={{ marginRight: '10px' }} />
-                  )}{' '}
-                  {/* Move the icon to the right */}
-                  {val.task}
-                </li>
-                <div className="flex flex-row space-x-5 mt-4">
-                  <button
-                    className="completeBtn bg-[#403d39] rounded-md px-2 py-2 mt-2 mb-2 text-white"
-                    onClick={() => completeTask(val.task)}
+        <div className="todo-list">
+          <ul className="mx-auto p-6 shadow-md rounded-md max-w-md ">
+            {toDoList.map((val, key) => {
+              return (
+                <div className="flex flex-col items-center" id="task" key={key}>
+                  <li
+                    style={{
+                      textDecoration: val.completed ? 'line-through' : 'none',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
                   >
-                    {val.completed ? 'Mark Incomplete' : 'Mark Complete'}
-                  </button>
-                  <button
-                    className="deleteBtn bg-[#eb5e28] rounded-md px-4 py-2 mt-2 mb-2 text-white"
-                    onClick={() => deleteTask(val.task)}
-                  >
-                    X
-                  </button>
-                  <button
-                    className="editBtn bg-[#403d39] rounded-md px-4 py-2 mt-2 mb-2 text-white"
-                    onClick={() => handleEdit(val.task)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="saveBtn bg-[#eb5e28] rounded-md px-4 py-2 mt-2 mb-2 text-white"
-                    onClick={handleSave}
-                  >
-                    Save
-                  </button>
+                    {val.reminder && (
+                      <FiCheckCircle style={{ marginRight: '10px' }} />
+                    )}{' '}
+                    {/* Move the icon to the right */}
+                    {val.task}
+                  </li>
+                  <div className="flex flex-row space-x-5 mt-4">
+                    <button
+                      className="completeBtn bg-[#403d39] rounded-md px-2 py-2 mt-2 mb-2 text-white"
+                      onClick={() => completeTask(val.task)}
+                    >
+                      {val.completed ? 'Mark Incomplete' : 'Mark Complete'}
+                    </button>
+                    <button
+                      className="deleteBtn bg-[#eb5e28] rounded-md px-4 py-2 mt-2 mb-2 text-white"
+                      onClick={() => deleteTask(val.task)}
+                    >
+                      X
+                    </button>
+                    <button
+                      className="editBtn bg-[#403d39] rounded-md px-4 py-2 mt-2 mb-2 text-white"
+                      onClick={() => handleEdit(val.task)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="saveBtn bg-[#eb5e28] rounded-md px-4 py-2 mt-2 mb-2 text-white"
+                      onClick={handleSave}
+                    >
+                      Save
+                    </button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </ul>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
